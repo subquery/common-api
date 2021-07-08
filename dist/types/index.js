3baddf39782659a85cc253cd23f44fe811d9298a
JQQQ <jiqiang90@hotmail.com>
2021-07-08 13:28:03 +1200

    Tidy up

--- dist/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// SPDX-License-Identifier: Apache-2.0
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-// Auto-generated , DO NOT EDIT
-tslib_1.__exportStar(require("./models"), exports);
-tslib_1.__exportStar(require("./interfaces"), exports);
--- dist/types/interfaces.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// SPDX-License-Identifier: Apache-2.0
-Object.defineProperty(exports, "__esModule", { value: true });
